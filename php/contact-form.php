e220fb7f13447e24a0bb263db6e3d4ebdaecc0a6
Derick Smith <globalsecurepayments@gmail.com>
2025-04-06 14:14:18 +0100

    bobs your uncle

--- php/contact-form.php
+++ php/contact-form.php
@@ -16,7 +16,7 @@ require 'php-mailer/src/SMTP.php';
 require 'php-mailer/src/Exception.php';
 
 // Step 1 - Enter your email address below.
-$email = 'you@domain.com';
+$email = 'admin@arabian-dragon.com';
 
 // If the e-mail is not working, change the debug option to 2 | $debug = 2;
 $debug = 0;
